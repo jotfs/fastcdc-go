21dff8878f83284d6b2ec7fd208a137134599cf8
Jakob Borg <jakob@kastelo.net>
2020-10-05 18:12:59 +0200

    Don't change the global table variable (fixes #4)
    Instead copy it when we want to use a new seed.
    
    Fixes the example test which in fact had incorrect output when run on
    its own, depending on the old behavior of someone else having clobbered
    the global table previously.

--- fastcdc.go
+++ fastcdc.go
@@ -32,6 +32,7 @@ type Chunker struct {
 	cursor int
 	offset int
 	eof    bool
+	table  *[256]uint64
 }
 
 // Options configures the options for the Chunker.
@@ -106,8 +107,13 @@ func NewChunker(rd io.Reader, opts Options) (*Chunker, error) {
 		return nil, err
 	}
 
-	for i := 0; i < len(table); i++ {
-		table[i] = table[i] ^ opts.Seed
+	localTable := &globalTable
+	if opts.Seed != 0 {
+		newTable := globalTable
+		for i := range newTable {
+			newTable[i] ^= opts.Seed
+		}
+		localTable = &newTable
 	}
 
 	normalization := opts.Normalization
@@ -127,6 +133,7 @@ func NewChunker(rd io.Reader, opts Options) (*Chunker, error) {
 		rd:       rd,
 		buf:      make([]byte, opts.BufSize),
 		cursor:   opts.BufSize,
+		table:    localTable,
 	}
 
 	return chunker, nil
@@ -194,14 +201,14 @@ func (c *Chunker) nextChunk(data []byte) (int, uint64) {
 	n := min(len(data), c.maxSize)
 
 	for ; i < min(n, c.normSize); i++ {
-		fp = (fp << 1) + table[data[i]]
+		fp = (fp << 1) + c.table[data[i]]
 		if (fp & c.maskS) == 0 {
 			return i + 1, fp
 		}
 	}
 
 	for ; i < n; i++ {
-		fp = (fp << 1) + table[data[i]]
+		fp = (fp << 1) + c.table[data[i]]
 		if (fp & c.maskL) == 0 {
 			return i + 1, fp
 		}
@@ -243,7 +250,7 @@ func min(a, b int) int {
 }
 
 // 256 random uint64s for the rolling hash function
-var table [256]uint64 = [256]uint64{
+var globalTable [256]uint64 = [256]uint64{
 	0xe80e8d55032474b3, 0x11b25b61f5924e15, 0x03aa5bd82a9eb669, 0xc45a153ef107a38c,
 	0xeac874b86f0f57b9, 0xa5ccedec95ec79c7, 0xe15a3320ad42ac0a, 0x5ed3583fa63cec15,
 	0xcd497bf624a4451d, 0xf9ade5b059683605, 0x773940c03fb11ca1, 0xa36b16e4a6ae15b2,
